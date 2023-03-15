5e0301e28dce2a064e109cd9b0df61f0da461b98
Hector Byström <bystrom.hector@elev.ga.ntig.se>
2023-03-15 12:24:20 +0100

    Initial commit

---
+++ src/Window.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    //attributes
+    public Window() {
+        this.setTitle("Bash a monster");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createLineBorder(Color.green,5));
+    }
+    //constructor
+
+    //methods
+
+//getters and setters
+}
