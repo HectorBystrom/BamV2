f73f518092ca80deed8570bd1b8fb99f6c6fb7ab
Hector Byström <bystrom.hector@elev.ga.ntig.se>
2023-03-15 12:36:20 +0100

    Första steget

--- src/HealthBar.java
+++ src/HealthBar.java
@@ -1,2 +1,34 @@
-public class HealthBar {
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class HealthBar extends JPanel implements MouseListener {
+
+    public HealthBar(Window window){
+
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -7,8 +7,18 @@ public class Window extends JFrame {
         this.setTitle("Bash a monster");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         JPanel panel = new JPanel();
+        GridLayout layout = new GridLayout(2,1);
+        panel.setLayout(layout);
         panel.setBorder(BorderFactory.createLineBorder(Color.green,5));
+
+        panel.add(new HealthBar(this));
+
+        this.getContentPane().add(panel);
+
+        this.pack();
+        this.setVisible(true);
     }
     //constructor
 
